1a8b183a7225c6cbbc29e60aa6a5d17768eab200
Jacob Lockhart <procoder165@gmail.com>
2022-08-28 13:12:14 -0400

    working on 5day

--- assets/script.js
+++ assets/script.js
@@ -38,22 +38,37 @@ fetch(geoLocation)
             var humidity = document.createElement("p");
             var icon = $("#image")
             image = data.current.weather[0].icon;
-            icon.attr("src", `https://openweathermap.org/img/wn/${image}@2x.png`)
+            icon.attr("src", `https://openweathermap.org/img/wn/${image}@2x.png`);
             icon.attr("height", "50");
+            var UV = document.createElement("p");
 
 
 
             date.textContent = moment().format("MMMM Do, YYYY");
             cityTitle.textContent = area.name;
-            temp.textContent = "temperature: " + data.current.temp + "F";
+            temp.textContent = "temperature: " + data.current.temp + "°F";
             wind.textContent = "wind: " + data.current.wind_speed + " MPH";
-            humidity.textContent = "humidity : " + data.current.humidity + "%";
+            humidity.textContent = "humidity: " + data.current.humidity + "%";
+            UV.textContent = "UV: " + data.current.uvi;
+            UV.setAttribute("class", "rounded")
+
+            if(data.current.uvi <= 3){
+                UV.setAttribute("class", "bg-success rounded p-2");
+            }
+            else if (data.current.uvi > 3 && data.current.uvi < 6){
+                UV.setAttribute("class", "bg-warning rounded p-2");
+            }
+            else {
+                UV.setAttribute("class", "bg-danger rounded p-2");
+            }
+
 
             $("#weather").append(date);
             $("#weather").append(cityTitle);
             $("#weather").append(temp);
             $("#weather").append(wind);
             $("#weather").append(humidity);
+            $("#weather").append(UV);
         })
     
 
@@ -63,12 +78,18 @@ fetch(geoLocation)
 
            var oneCall = `http://api.openweathermap.org/data/2.5/forecast?lat=${area.lat}&lon=${area.lon}&appid=a83cd16f45a517250a52f83592bb80be&cnt=6&units=imperial`;
 
+
           fetch(oneCall)
           .then(function(response){
                return response.json();
            })
            .then(function (data){
             console.log(data);
+
+            for(var i = )
+
+
+
             
           })
 
